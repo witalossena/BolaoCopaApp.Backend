22ad466d529f7b8d718740fc199d1720aba411e7
witalossena <sousawitalo@gmail.com>
2026-06-01 12:07:15 -0300

    Fix: updated database port to 54322

--- BolaoCopaApp.Infrastructure/Persistence/BolaoDbContextFactory.cs
+++ BolaoCopaApp.Infrastructure/Persistence/BolaoDbContextFactory.cs
@@ -8,7 +8,7 @@ public class BolaoDbContextFactory : IDesignTimeDbContextFactory<BolaoDbContext>
     public BolaoDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<BolaoDbContext>();
-        optionsBuilder.UseNpgsql("Host=187.45.255.12;Database=CopaBolao;Username=copa_bolao_user;Password=XkYuredgcmiu8QaK2506");
+        optionsBuilder.UseNpgsql("Host=187.45.255.12;Port=54322;Database=CopaBolao;Username=copa_bolao_user;Password=XkYuredgcmiu8QaK2506");
 
         return new BolaoDbContext(optionsBuilder.Options);
     }
